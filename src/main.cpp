ebbf0f5d988ad84401b9da25c34e3bb8de00bb56
David Dubois <dexcelstraun7@gmail.com>
2019-11-26 23:42:43 -0800

    [main] Add basic main loop

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,13 @@ int main(int argc, const char** argv) {
 #endif
 	
 	nova::renderer::NovaRenderer renderer(settings);
+
+	renderer.set_num_meshes(32);	// Best guess, should fix when we know more
 	
+	nova::renderer::Window& window = renderer.get_engine()->get_window();
+	while(!window.should_close()) {
+		// Main loop!
+	}
 	
 	return 0;
 }
