5978fa4ba5fcaf8b9a4cf65d8af5c0de01ebf7e3
dethraid <dexcelstraun7@gmail.com>
2019-12-01 10:21:40 -0800

    [world] Bookkepping when adding/removing an entity, and tick all entities in the world

--- src/ec/entity.cpp
+++ src/ec/entity.cpp
@@ -2,12 +2,17 @@
 
 #include "component.hpp"
 
-namespace  nova::ec {
-    void Entity::tick(const double delta_time)
-	{
-		for(auto* component : components)
-		{
-			component->tick(delta_time);
-		}
-	}
-}
+namespace nova::ec {
+    Entity::Entity() {
+        id = next_id;
+        next_id++;
+    }
+
+    uint64_t Entity::get_id() const { return id; }
+
+    void Entity::tick(const double delta_time) {
+        for(auto* component : components) {
+            component->tick(delta_time);
+        }
+    }
+} // namespace nova::ec
