5fc5064260903b7b01f4c44341723b87cf3a743c
David Dubois <dexcelstraun7@gmail.com>
2020-01-07 00:51:41 -0800

    [renderpack::Simple] Vertex fields are no longer in pipeline create info

--- data/shaderpacks/Simple/shaders/gui.pixel.hlsl
+++ data/shaderpacks/Simple/shaders/gui.pixel.hlsl
@@ -8,10 +8,10 @@ struct VsOutput {
     float4 position : SV_POSITION;
     float2 uv : TEXCOORD;
     float4 color : COLOR;
-    uint texture_index : INDEX;
+    float4 data : INDEX;
 };
 
 float4 main(VsOutput input) : SV_Target {
-    float4 texture_color = ui_textures[input.texture_index].Sample(ui_sampler, input.uv);
+    float4 texture_color = ui_textures[input.data.x].Sample(ui_sampler, input.uv);
     return input.color * texture_color;
 }
--- data/shaderpacks/Simple/shaders/gui.vertex.hlsl
+++ data/shaderpacks/Simple/shaders/gui.vertex.hlsl
@@ -2,21 +2,21 @@ struct VsInput {
     float2 position : POSITION;
     float2 uv : TEXCOORD;
     uint color : COLOR;
-    uint texture_index : INDEX;
+    float4 data : INDEX;
 };
 
 struct VsOutput {
     float4 position : SV_POSITION;
     float2 uv : TEXCOORD;
     float4 color : COLOR;
-    uint texture_index : INDEX;
+    float4 data : INDEX;
 };
 
 struct UiConstants {
     float4x4 view_matrix;
 };
 
-[[vk::binding(0)]]
+[[vk::binding(0, 0)]]
 ConstantBuffer<UiConstants> constants   : register(b0, space0);
 
 VsOutput main(VsInput input) {
@@ -33,7 +33,7 @@ VsOutput main(VsInput input) {
     uint alpha = input.color & 0xFF;
     output.color = float4(red, green, blue, alpha) / 255.0f;
 
-    output.texture_index = input.texture_index;
+    output.data = input.data;
 
     return output;
 }
