2a005bf4bbbb8f9ba0316677c9e4e7783d5a36c5
David Dubois <dexcelstraun7@gmail.com>
2020-02-02 21:12:23 -0800

    [main] Convert to using Rex things

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 
 #include <nova_renderer/nova_renderer.hpp>
 
-#include "nova_renderer/util/logger.hpp"
-
 #include "ec/entity.hpp"
 #include "renderer/nk_backend.hpp"
 #include "ui/train_selection_panel.hpp"
@@ -15,47 +13,10 @@ using namespace renderer;
 using namespace filesystem;
 using namespace ec;
 
-void setup_logger() {
-    // TODO: Nova needs a better way to handle this
-    auto error_log = std::make_shared<std::ofstream>();
-    error_log->open("test_error_log.log");
-    auto test_log = std::make_shared<std::ofstream>("test_log.log");
-    auto& log = Logger::instance;
-    log.add_log_handler(TRACE, [test_log](auto msg) {
-        std::cout << "TRACE: " << msg.c_str() << std::endl;
-        *test_log << "TRACE: " << msg.c_str() << std::endl;
-    });
-    log.add_log_handler(DEBUG, [test_log](auto msg) {
-        std::cout << "DEBUG: " << msg.c_str() << std::endl;
-        *test_log << "DEBUG: " << msg.c_str() << std::endl;
-    });
-    log.add_log_handler(INFO, [test_log](auto msg) {
-        std::cout << "INFO: " << msg.c_str() << std::endl;
-        *test_log << "INFO: " << msg.c_str() << std::endl;
-    });
-    log.add_log_handler(WARN, [test_log](auto msg) {
-        std::cerr << "WARN: " << msg.c_str() << std::endl;
-        *test_log << "WARN: " << msg.c_str() << std::endl;
-    });
-    log.add_log_handler(ERROR, [test_log, error_log](auto msg) {
-        std::cerr << "ERROR: " << msg.c_str() << std::endl;
-        *error_log << "ERROR: " << msg.c_str() << std::endl << std::flush;
-        *test_log << "ERROR: " << msg.c_str() << std::endl;
-    });
-    log.add_log_handler(FATAL, [test_log, error_log](auto msg) {
-        std::cerr << "FATAL: " << msg.c_str() << std::endl;
-        *error_log << "FATAL: " << msg.c_str() << std::endl << std::flush;
-        *test_log << "FATAL: " << msg.c_str() << std::endl;
-    });
-    log.add_log_handler(MAX_LEVEL, [test_log, error_log](auto msg) {
-        std::cerr << "MAX_LEVEL: " << msg.c_str() << std::endl;
-        *error_log << "MAX_LEVEL: " << msg.c_str() << std::endl << std::flush;
-        *test_log << "MAX_LEVEL: " << msg.c_str() << std::endl;
-    });
-}
+RX_LOG("BestFriend", logger);
 
 int main(int argc, const char** argv) {
-    setup_logger();
+    init_rex();
 
     NovaSettings settings;
 #if NOVA_DEBUG
@@ -73,10 +34,13 @@ int main(int argc, const char** argv) {
 
     renderer.set_num_meshes(32); // Best guess, should fix when we know more
 
-    std::unique_ptr<World> world = std::make_unique<World>();
+    auto* allocator = &rx::memory::g_system_allocator;
 
-    auto* nuklear_device = renderer.set_ui_renderpass(std::make_unique<NuklearDevice>(renderer),
-                                                      NuklearDevice::get_create_info());
+    auto* world = allocator->create<World>();
+
+    auto* nuklear_device = allocator->create<NuklearDevice>(renderer);
+
+    renderer.set_ui_renderpass(nuklear_device, NuklearDevice::get_create_info());
 
     renderer.load_renderpack("Simple");
 
@@ -88,8 +52,8 @@ int main(int argc, const char** argv) {
 
     const auto& window = renderer.get_window();
 
-    auto frame_start_time = static_cast<double>(std::clock());
-    auto frame_end_time = static_cast<double>(std::clock());
+    auto frame_start_time = static_cast<double>(clock());
+    auto frame_end_time = static_cast<double>(clock());
     auto last_frame_duration = (frame_end_time - frame_start_time) / CLOCKS_PER_SEC;
 
     // Number of frames since program start
@@ -98,17 +62,17 @@ int main(int argc, const char** argv) {
     while(!window.should_close()) {
         // Main loop!
 
-        frame_start_time = static_cast<double>(std::clock());
+        frame_start_time = static_cast<double>(clock());
 
         if(frame_counter % 100 == 0) {
-            NOVA_LOG(INFO) << "Frame " << frame_counter << " took " << last_frame_duration << "ms\n";
+            logger(rx::log::level::k_info, "Frame %u took %ums", frame_counter, last_frame_duration);
         }
 
         world->tick(last_frame_duration);
 
         renderer.execute_frame();
 
-        frame_end_time = static_cast<double>(std::clock());
+        frame_end_time = static_cast<double>(clock());
         last_frame_duration = (frame_end_time - frame_start_time) / CLOCKS_PER_SEC;
         frame_counter++;
     }
