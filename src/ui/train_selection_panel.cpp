9909d8e3caa63f3978e6c9a7d21fed2d58d2a6a7
David Dubois <dexcelstraun7@gmail.com>
2020-02-10 19:56:51 -0800

    [ui] Please select a train

--- src/ui/train_selection_panel.cpp
+++ src/ui/train_selection_panel.cpp
@@ -6,27 +6,43 @@ namespace nova::bf {
     void TrainSelectionPanel::draw() {
         const bool is_train_selected = !last_loaded_train.is_empty();
 
-        if(nk_begin(ctx,
+        /*if(nk_begin(ctx,
                     "Train Selection",
-                    {50, 50, 220, 200},
+                    {5, 5, 220, 200},
                     NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_CLOSABLE)) {
-            nk_layout_row_static(ctx, 30, 80, 1);
+            nk_layout_row_dynamic(ctx, 0, 1);
 
             // Display the currently selected train file
             // Button to select a different train file, should open an OS-native file picker
             // Maybe some import options for edge smoothing? These should be cached, as should all generated data, so that loading a train
             // file can be very fast
 
-            const auto selected_train_label = [&]() -> rx::string {
-                if(is_train_selected) {
-                    return rx::string::format("Selected train file: %s", last_loaded_train);
+            nk_text(ctx, selected_train_label.data(), 0, 0);
+        }
+        nk_end(ctx);*/
 
-                } else {
-                    return "Please select a train";
-                }
-            }();
+        const auto selected_train_label = [&]() -> rx::string {
+            if(is_train_selected) {
+                return rx::string::format("Selected train file: %s", last_loaded_train);
 
-            nk_label(ctx, selected_train_label.data(), 0);
+            } else {
+                return "Please select a train";
+            }
+        }();
+
+        if(nk_begin(ctx,
+                    "Train Selection",
+                    {5, 5, 425, 200},
+                    NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_CLOSABLE)) {
+
+            // fixed widget pixel width
+            nk_layout_row_static(ctx, 0, 400, 1);
+
+            nk_label(ctx, selected_train_label.data(), NK_TEXT_LEFT);
+
+            if(nk_button_label(ctx, "Choose train file")) {
+                // event handling
+            }
         }
         nk_end(ctx);
     }
