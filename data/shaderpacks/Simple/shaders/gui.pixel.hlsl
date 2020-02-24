3f87fd01b244d153ef9ef16d84713e6a5433e5be
David Dubois <dexcelstraun7@gmail.com>
2020-02-23 16:56:28 -0800

    [shaders] Fix UI color endianness

--- data/shaderpacks/Simple/shaders/gui.pixel.hlsl
+++ data/shaderpacks/Simple/shaders/gui.pixel.hlsl
@@ -12,10 +12,11 @@ struct VsOutput {
 };
 
 float4 main(VsOutput input) : SV_Target {
-    uint red = (input.color >> 24) & 0xFF;
-    uint green = (input.color >> 16) & 0xFF;
-    uint blue = (input.color >> 8) & 0xFF;
-    uint alpha = input.color & 0xFF;
+    uint red = input.color & 0xFF;
+    uint green = (input.color >> 8) & 0xFF;
+    uint blue = (input.color >> 16) & 0xFF;
+    uint alpha = (input.color >> 24) & 0xFF;
+    
     float4 vertex_color = float4(red, green, blue, alpha) / 255.0f;
 
     float4 texture_color = ui_textures[input.texture_id].Sample(ui_sampler, input.uv);
