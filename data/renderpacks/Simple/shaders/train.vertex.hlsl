75e4edb93c9b7ee9f27c9d9518f07b5c2bf606e8
David Dubois <dexcelstraun7@gmail.com>
2020-03-15 18:34:08 -0700

    [shaders] Update one shader to use the pipeline interface

--- data/renderpacks/Simple/shaders/train.vertex.hlsl
+++ data/renderpacks/Simple/shaders/train.vertex.hlsl
@@ -1,58 +1,8 @@
-struct Camera {
-    float4x4 view;
-    float4x4 projection;
-};
-
-struct UiMaterial {
+struct MaterialData {
     uint texture_idx;
 };
 
-/*!
- * \brief All the push constants that are available to a shader that uses the standard pipeline layout
- */
-[[vk::push_constant]]
-struct StandardPushConstants {
-    /*!
-     * \brief Index of the material data for the current draw
-     */
-    uint material_index;
-} constants;
-
-/*!
- * \brief Array of all the materials 
- */
-[[vk::binding(0, 0)]]
-StructuredBuffer<Camera[256]> cameras : register (b0);
-
-/*!
- * \brief Array of all the materials 
- */
-[[vk::binding(1, 0)]]
-StructuredBuffer<UiMaterial[64]> material_buffer : register (b1);
-
-/*!
- * \brief Point sampler you can use to sample any texture
- */
-[[vk::binding(2, 0)]]
-SamplerState point_sampler : register(s0);
-
-/*!
- * \brief Bilinear sampler you can use to sample any texture
- */
-[[vk::binding(3, 0)]]
-SamplerState bilinear_filter : register(s1);
-
-/*!
- * \brief Trilinear sampler you can use to sample any texture
- */
-[[vk::binding(4, 0)]]
-SamplerState trilinear_filter : register(s3);
-
-/*!
- * \brief Array of all the textures that are available for a shader to sample from
- */
-[[vk::binding(5, 0)]]
-Texture2D textures[] : register(t0);
+#include "nova/standard_pipeline_layout.hlsl"
 
 struct VsInput {
     float3 position : POSITION;
@@ -68,8 +18,8 @@ struct VsOutput {
 VsOutput main(VsInput input) {
     VsOutput output;
 
-    Camera camera = camera_matrices.cameras[push_constants.camera_index];
-    float4x4 vp = mul(camera.view_matrix, camera.projection_matrix);
+    Camera camera = cameras.Load(constants.camera_index);
+    float4x4 vp = mul(camera.view, camera.projection);
 
     output.position = mul(float4(input.position, 1), vp);
     output.normal = input.normal;
