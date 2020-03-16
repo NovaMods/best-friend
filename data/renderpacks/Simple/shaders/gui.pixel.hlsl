92f5c8457f1df647747de58109b1b4fd6b166bcf
David Dubois <dexcelstraun7@gmail.com>
2020-03-15 20:42:13 -0700

    [shaders] Update shaders to use the standard pipeline layout

--- data/renderpacks/Simple/shaders/gui.pixel.hlsl
+++ data/renderpacks/Simple/shaders/gui.pixel.hlsl
@@ -1,58 +1,8 @@
-struct Camera {
-    float4x4 view;
-    float4x4 projection;
-};
-
-struct UiMaterial {
+struct MaterialData {
     uint texture_idx;
 };
 
-/*!
- * \brief All the push constants that are available to a shader that uses the standard pipeline layout
- */
-[[vk::push_constant]]
-struct StandardPushConstants {
-    /*!
-     * \brief Index of the material data for the current draw
-     */
-    uint material_index;
-} constants;
-
-/*!
- * \brief Array of all the materials 
- */
-[[vk::binding(0, 0)]]
-StructuredBuffer<Camera[256]> cameras : register (b0);
-
-/*!
- * \brief Array of all the materials 
- */
-[[vk::binding(1, 0)]]
-StructuredBuffer<UiMaterial[64]> material_buffer : register (b1);
-
-/*!
- * \brief Point sampler you can use to sample any texture
- */
-[[vk::binding(2, 0)]]
-SamplerState point_sampler : register(s0);
-
-/*!
- * \brief Bilinear sampler you can use to sample any texture
- */
-[[vk::binding(3, 0)]]
-SamplerState bilinear_filter : register(s1);
-
-/*!
- * \brief Trilinear sampler you can use to sample any texture
- */
-[[vk::binding(4, 0)]]
-SamplerState trilinear_filter : register(s3);
-
-/*!
- * \brief Array of all the textures that are available for a shader to sample from
- */
-[[vk::binding(5, 0)]]
-Texture2D textures[] : register(t0);
+#include "nova/standard_pipeline_layout.hlsl"
 
 struct VsOutput {
     float4 position : SV_POSITION;
